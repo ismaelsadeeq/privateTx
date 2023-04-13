f0b6a74df13f66dfe6cd0ce04989284e554b92fe
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-04-13 15:23:13 +0100

    feature: add detect change outputs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.peelingTransaction = exports.commonInputs = exports.checkAddressReuse = void 0;
+exports.detectChange = exports.peelingTransaction = exports.commonInputs = exports.checkAddressReuse = void 0;
 var address_reuse_1 = require("./lib/address-reuse");
 Object.defineProperty(exports, "checkAddressReuse", { enumerable: true, get: function () { return address_reuse_1.checkAddressReuse; } });
 var common_input_1 = require("./lib/common-input");
 Object.defineProperty(exports, "commonInputs", { enumerable: true, get: function () { return common_input_1.commonInputs; } });
 var peeling_1 = require("./lib/peeling");
 Object.defineProperty(exports, "peelingTransaction", { enumerable: true, get: function () { return peeling_1.peelingTransaction; } });
+var detect_change_1 = require("./lib/detect-change");
+Object.defineProperty(exports, "detectChange", { enumerable: true, get: function () { return detect_change_1.detectChange; } });
--- ts_src/index.ts
+++ ts_src/index.ts
@@ -1,5 +1,6 @@
 export { checkAddressReuse } from './lib/address-reuse';
-export { commonInputs } from './lib/common-input'
-export { peelingTransaction } from './lib/peeling'
+export { commonInputs } from './lib/common-input';
+export { peelingTransaction } from './lib/peeling';
+export { detectChange } from './lib/detect-change';
 
  
